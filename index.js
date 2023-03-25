64261e1b11ebe15b043d4348e6108c685a439e8b
Sonia-saini <105914181+Sonia-saini@users.noreply.github.com>
2023-03-25 11:11:19 +0530

    cors error resoloved

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express=require("express");
 const app=express();
 require("dotenv").config();
-
 const cors=require("cors");
 const { connection } = require("./src/config/db");
 const { userRouter } = require("./src/user/Userroute");
